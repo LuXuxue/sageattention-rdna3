8ffc3d23b23dba466703a49f98258e5db031aff6
LuXuxue <icekinfe@163.com>
2026-08-10 01:02:06 +0800

    add benchmark result

--- benchmark_attn.bat
+++ benchmark_attn.bat
@@ -1,5 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
-set SAGEATTN_BACKEND=native
+if exist "%~dp0python_embeded" (set python_embeded_path=%~dp0python_embeded) else (set python_embeded_path=E:\python_embeded)
+
 rd /s /q %UserProfile%\.triton\cache >nul 2>&1
-python.exe benchmark_attn.py
+set SAGEATTN_BACKEND=triton
+%python_embeded_path%\python.exe benchmark_attn.py
+%python_embeded_path%\python.exe benchmark_attn.py>benchmark_attn-result-triton.txt 2>&1
+
+rd /s /q %UserProfile%\.triton\cache >nul 2>&1
+set SAGEATTN_BACKEND=native
+%python_embeded_path%\python.exe benchmark_attn.py
+%python_embeded_path%\python.exe benchmark_attn.py>benchmark_attn-result-native.txt 2>&1
